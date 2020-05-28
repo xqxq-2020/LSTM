91412410fdf0584f466deed61b86723d22c07692
zhangxi1997 <zhangxi2019@ia.ac.cn>
2020-05-28 22:59:31 +0800

    add main.py

---
+++ main.py
@@ -0,0 +1,22 @@
+import os
+import argparse
+
+parser = argparse.ArgumentParser()
+parser.add_argument('-g', '--gpu', default = [], nargs='+', type=str, help='Specify GPU id.')
+parser.add_argument( '-e', '--epoch', type=int, help='train epoch number')
+parser.add_argument( '--checkpoint', default = './', type=str, help='checkpoint.')
+parser.add_argument( '--seed', default = 1, type=int, help='seed for pytorch init')
+parser.add_argument( '-b','--bidirection', action='store_true', help='use bi-direction lstm or not')
+args = parser.parse_args()
+print(args)
+
+torch.manual_seed(args.seed)
+
+if args.gpu:
+    os.environ['CUDA_VISIBLE_DEVICES'] = args.gpu[0]
+
+model = LSTM_Model(args)
+model.train()
+
+#lstm_encoder = LSTM_CRF_model(args)
+#LSTM_CRF_model.train()
