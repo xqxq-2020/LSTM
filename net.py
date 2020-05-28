688403af036731bf4d846fc62c969ff8d02e297a
zhangxi1997 <zhangxi2019@ia.ac.cn>
2020-05-28 21:40:31 +0800

    add dropout and tanh

--- net.py
+++ net.py
@@ -22,24 +22,31 @@ class LSTMTagger(nn.Module):
                             dropout = self.drop_rate, batch_first=True, bidirectional=self.bidirect)
         ## 如果batch_first为True，输入输出数据格式是(batch, seq_len, feature)
         ## 为False，输入输出数据格式是(seq_len, batch, feature)，
-        
+        self.dropout = nn.Dropout(self.config.DROP_RATE)
+
         if self.bidirect:
-            self.hidden2tag = nn.Linear(self.hidden_dim*2,self.tagset_size)
+            self.hidden2tag = nn.Linear(self.hidden_dim*2, self.tagset_size)
         else:
-            self.hidden2tag = nn.Linear(self.hidden_dim,self.tagset_size)# hidden to tag
+            self.hidden2tag = nn.Linear(self.hidden_dim, self.tagset_size)# hidden to tag
 
     def forward(self, sentence, lens_batch, is_test=False):
         embeds = self.word_embeddings(sentence)# sentence 是词seq的索引
-        #self.input_tensor = embeds.view(self.batch_size, len(sentence) // self.batch_size, -1)
+
         if is_test==False:
             self.input_tensor = nn.utils.rnn.pack_padded_sequence(embeds, lens_batch, batch_first=True)
-            lstm_out,self.hidden = self.lstm(self.input_tensor)#, self.hidden)
-            lstm_out_pack = nn.utils.rnn.pad_packed_sequence(lstm_out, batch_first=True)
-            # lstm_out_pack[0] 才是要用的tensor，他是个元组 
-            tag_space = self.hidden2tag(lstm_out_pack[0])#.view(len(sentence), -1)) (batch, seq_len, feature_dim)
+            lstm_out,self.hidden = self.lstm(self.input_tensor)
+            lstm_out_pack,_ = nn.utils.rnn.pad_packed_sequence(lstm_out, batch_first=True)
+
+            x = self.dropout(lstm_out_pack)#add
+            x = torch.tanh(x)#add
+            tag_space = self.hidden2tag(lstm_out_pack)
+
         else:
             lstm_out, self.hidden = self.lstm(embeds.view(len(sentence), 1, -1))
+            x = self.dropout(lstm_out)
+            x = torch.tanh(x)
             tag_space = self.hidden2tag(lstm_out.view(len(sentence), -1))
-        tag_scores = F.log_softmax(tag_space, dim=1)
+
+        #tag_scores = F.log_softmax(tag_space, dim=1)
         
         return tag_scores
