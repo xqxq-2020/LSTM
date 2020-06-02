4b3cef6d6659add65d4fd62e53d97419afaa64c4
zhangxi1997 <zhangxi2019@ia.ac.cn>
2020-06-02 09:45:59 +0800

    test

--- test.py
+++ test.py
@@ -8,15 +8,14 @@ from lstm_crf import BiLSTM_CRF
 
 parser = argparse.ArgumentParser()
 parser.add_argument('-g', '--gpu', default = [], nargs='+', type=str, help='Specify GPU id.')
-parser.add_argument( '-e', '--epoch', type=int, help='train epoch number')
-parser.add_argument( '--checkpoint', default = './', type=str, help='checkpoint.')
+parser.add_argument( '-e', '--epoch', default = 1, type=int, help='train epoch number')
+parser.add_argument( '--checkpoint', default = 'checkpoint/', type=str, help='checkpoint.')
 parser.add_argument( '--seed', default = 1, type=int, help='seed for pytorch init')
 parser.add_argument( '-b','--bidirection', action='store_true', help='use bi-direction lstm or not')
 parser.add_argument('--weighted_tag', action='store_true',help='use wighted loss or not' )
 parser.add_argument('--crf', action='store_true',help='use crf or not' )
 
 args = parser.parse_args()
-print(args)
 
 if args.gpu:
     os.environ['CUDA_VISIBLE_DEVICES'] = args.gpu[0]
@@ -25,20 +24,26 @@ dataset_tool = Dataset(config)
 
 model = BiLSTM_CRF(dataset_tool.word_to_ix_length,config.TAG_to_ix_crf,config.EMBEDDING_DIM,config.HIDDEN_DIM)
 model.load_state_dict(torch.load(args.checkpoint+"/epoch_max_accuracy.pkl"))
-print("load successful")
-
-#"越权/vi  违规/vi  里森/nr  主管/vt  炒/vt  期货/n  资不抵债/l  [巴林/ns  银行/n]nt  告/vt  终结/vi  "
+print("Loading model...")
 
 sentence = "越权 违规 里森 主管 炒 期货 资不抵债 巴林 银行 告 终结"
+gt = ['v','v','n','v','v','n','l','n','n','v','v']
 
 if __name__ == '__main__':
     word_str = sentence.split()
-    print(word_str)
+    print("Sentence:\n",sentence)
+    print("Ground Truth:")
+    print(gt)
     inputs = dataset_tool.prepare_sequence(word_str,dataset_tool.word_to_ix)
     inputs = torch.tensor(inputs, dtype=torch.long)
-    inputs.cuda(0)
-    model.cuda(0)
-    score, pred_tag = model.lstm_crf_model(inputs)
-
-    print(score,pred_tag)
-
+    inputs = inputs.cuda(0)
+    model = model.cuda(0)
+    with torch.no_grad():
+        score, pred_tag = model(inputs)
+    
+    print("Result:")
+    result = []
+    for item in pred_tag:
+        result.append(list(config.TAG_to_ix.keys())[item])
+    print(result)
+    print("score:",score.cpu().numpy())
